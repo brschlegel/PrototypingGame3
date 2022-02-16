1703fcb77dacb947d5af0f330ccfb1469b9f9e1c
zach-c-erickson <zce1382@g.rit.edu>
2022-02-16 16:57:06 -0500

    Level 2

--- PrototypingGame3/Assets/Scripts/Door.cs
+++ PrototypingGame3/Assets/Scripts/Door.cs
@@ -6,6 +6,16 @@ using TarodevController;
 public class Door : MonoBehaviour
 {
     private Animator animator;
+
+    // Gems found in the level
+    public Gem gem1;
+    public Gem gem2;
+    public Gem gem3;
+
+    private bool isUnlocked = false;
+
+    public GameManager gameManager;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,13 +25,27 @@ public class Door : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // If player collects all gems, change sprite on door to unlocked sprite
+        if (gem1.collected && gem2.collected  && gem3.collected)
+        {
+            animator.SetBool("Unlocked_b", true);
+            isUnlocked = true;
+        }
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.TryGetComponent(out PlayerController controller))
+        // if player approaches unlocked door, play door opening animation and end level
+        if (collision.TryGetComponent(out PlayerController controller) && isUnlocked)
         {           
             animator.SetTrigger("Door_Open");
+
+            StartCoroutine("EndLevel");
         }
+    }
+
+    public IEnumerator EndLevel()
+    {
+        yield return new WaitForSeconds(1f);
+        gameManager.EndLevel();
     }
 }
