5930d45ef64f1fccd3b063992845e2b3686c30fb
Candygonemad <candygonemad@gmail.com>
2022-02-17 12:57:59 -0500

    Chromatic aberration on death working

--- PrototypingGame3/Assets/Scripts/Shaker.cs
+++ PrototypingGame3/Assets/Scripts/Shaker.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.PostProcessing;
 
 public class Shaker : MonoBehaviour
 {
@@ -13,11 +15,18 @@ public class Shaker : MonoBehaviour
 
     public bool isDone;
 
+    public PostProcessVolume postProcess;
+    private ChromaticAberration aberration;
+
     // Start is called before the first frame update
     void Start()
     {
         target = gameObject.transform;
         initialPos = transform.position;
+
+        //Get aberration
+        Debug.Log("Start");
+        postProcess.profile.TryGetSettings(out aberration);
     }
 
     // Update is called once per frame
@@ -31,6 +40,7 @@ public class Shaker : MonoBehaviour
         isDone = false;
         float startTime = Time.realtimeSinceStartup;
         float power = intensity;
+        
 
         Debug.Log("Start coroutine");
         while(Time.realtimeSinceStartup < startTime + duration)
@@ -38,9 +48,18 @@ public class Shaker : MonoBehaviour
             Vector3 randomPoint = new Vector3(Random.Range(-1f, 1f) * power, Random.Range(-1f, 1f) * power, initialPos.z);
             target.position = initialPos + randomPoint;
             power = Mathf.MoveTowards(power, 0, shakeFadeTime * Time.deltaTime);
+
+            //Get the value of the chromatic aberration based on how what point in the camera shake it is
+            aberration.intensity.value = 1;
+            Debug.Log("Aberration: " + aberration.intensity.value);
+
             yield return null;
         }
         Debug.Log("Finish Coroutine");
+
+        //Set it back to its default value
+        aberration.intensity.value = 0;
+
         target.position = initialPos;
         isDone = true;
     }
